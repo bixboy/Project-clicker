d111e56c2f04155b88748bebc2f6c8633dae79d5
Volodia <volodiabussereau@gmail.com>
2023-10-24 14:24:54 +0200

    Changed a bit of UI

--- Assets/ScriptableObjects/Upgrade.cs
+++ Assets/ScriptableObjects/Upgrade.cs
@@ -13,9 +13,11 @@ public class Upgrade : ScriptableObject
     [SerializeField] private Sprite _sprite;
 
     public StatName Name => _statName;
-    public float Amount => _amount;
+    public float Amount => _amount + _level * _amount;
+
+    public float NextAmount => _amount + (_level + 1) * _amount;
     public int Level => _level;
-    public float value => _amount + _level * _amount;
+    public float BaseAmount => _amount;
     public int Cost => (int)(_cost * (0.2*_level + 1));
     public Sprite Sprite => _sprite;
     public void Buy()
--- Assets/Scripts/UpgradeUI.cs
+++ Assets/Scripts/UpgradeUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +11,8 @@ public class UpgradeUI : MonoBehaviour
     [Header("ObjectToReference")]
     [SerializeField] private TMP_Text _upgradeName;
     [SerializeField] private TMP_Text _upgradeCost;
+    [SerializeField] private TMP_Text _currentAmount;
+    [SerializeField] private TMP_Text _nextAmount;
     [SerializeField] private Image _upgradeSprite;
     private UpgradeManager _upgradeManager;
     private Upgrade _upgrade;
@@ -24,7 +27,15 @@ public class UpgradeUI : MonoBehaviour
         _upgrade = _upgradeManager.GetUpgradeByName(_displayedUpgrade);
         _upgradeName.text = _displayedUpgrade.ToString();
         _upgradeCost.text = _upgrade.Cost.ToString();
+        _currentAmount.text = _upgrade.Amount.ToString(CultureInfo.InvariantCulture);
+        _nextAmount.text = _upgrade.NextAmount.ToString(CultureInfo.InvariantCulture);
         _upgradeSprite.sprite = _upgrade.Sprite;
+    }
+
+    public void Buy(StatName statName)
+    {
+        _upgradeManager.Buy(statName);
+        UpdateUI();
     }
 
 
